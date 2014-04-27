a21ef0fdb463f53d015c5357dcf220270e5931df
showtimeshane <shanetimothyfoster@gmail.com>
2014-04-27 08:32:45 -0700

    First commit on Dancelinked

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dancelinked::Application.config.secret_key_base = '2cd4117c0c948a899ad4418e4aee23aaa0a5a6be6aa9a29c0456929aa3e9a6a31710c575c976b0bcf8b33e5400062e813144747f36f4b0284b9ae0b5d253df5f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Dancelinked::Application.config.session_store :cookie_store, key: '_dancelinked_session'
