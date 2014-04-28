e23046bef14b6e4028a7e4eeb9c3453bd5187fba
showtimeshane <shanetimothyfoster@gmail.com>
2014-04-27 20:00:02 -0700

    Added devise gem for Authentication. Create status update MVC architecture while following Treehouse tutorial.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Dancelinked::Application.routes.draw do
+
+  resources :statuses
+
+  devise_for :users
+  root to: "statuses#index"
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
